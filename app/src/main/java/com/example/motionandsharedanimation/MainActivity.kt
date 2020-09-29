256bf9f986750a2c7f0a1499a7fb970f9d03be6c
pritesh <priteshvishwakarma@gmail.com>
2020-09-29 11:16:07 +0530

    global back button added

--- app/src/main/java/com/example/motionandsharedanimation/MainActivity.kt
+++ app/src/main/java/com/example/motionandsharedanimation/MainActivity.kt
@@ -3,22 +3,34 @@ package com.example.motionandsharedanimation
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.motionandsharedanimation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    var navigationHostFragment : NavHostFragment? = null
+
+    private lateinit var b: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        b = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b.root)
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navigationHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navigationHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val appBarConfiguration = AppBarConfiguration(setOf(R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navigationHostFragment.navController, appBarConfiguration)
-        navView.setupWithNavController(navigationHostFragment.navController)
+        setupActionBarWithNavController(navigationHostFragment!!.navController, appBarConfiguration)
+        navView.setupWithNavController(navigationHostFragment!!.navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navigationHostFragment!!.navController,null)
     }
 }
